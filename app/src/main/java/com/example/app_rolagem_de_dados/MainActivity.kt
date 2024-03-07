7c41c26cc6f41a9b884f1b198b1eb253424f70bb
Joa0DeL1ma <161715327+Joa0DeL1ma@users.noreply.github.com>
2024-03-07 18:03:54 -0300

    Adicionado primeiro dado

--- app/src/main/java/com/example/app_rolagem_de_dados/MainActivity.kt
+++ app/src/main/java/com/example/app_rolagem_de_dados/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.app_rolagem_de_dados
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -13,8 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.app_rolagem_de_dados.ui.theme.App_Rolagem_de_DadosTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +39,9 @@ fun DadoComBotaoEImagem(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = "1")
         Button(onClick = { /*TODO*/ }) {
+            Spacer(modifier = Modifier.height(16.dp))
             Text(text = stringResource(R.string.roll))
         }
     }
@@ -45,6 +52,6 @@ fun DadoComBotaoEImagem(modifier: Modifier = Modifier) {
 fun AppRolagemDado() {
     DadoComBotaoEImagem(
         modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center))
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center))
 }
